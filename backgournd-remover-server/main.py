8fef28efc20ef4d41b071e9114dd07e87a1a1d9c
Essadi <yasine.esadi@gmail.com>
2023-09-28 14:23:04 +0100

    cleaning the code (Added).

--- backgournd-remover-server/main.py
+++ backgournd-remover-server/main.py
@@ -1,9 +1,6 @@
 from rembg import remove
 from fastapi import FastAPI, File, UploadFile
 import uuid
-import os
-from random import randint
-# from fastapi.responses import FileResponse
 
 
 
@@ -27,32 +24,5 @@ async def create_upload_file(file: UploadFile = File(...)):
             output = remove(input)
             o.write(output)
     return output_path
-
-
-# @app.get("/show/")
-# async def read_random_file():
- 
-#     # get random file from the image directory
-#     files = os.listdir(IMAGEDIR)
-#     random_index = randint(0, len(files) - 1)
- 
-#     path = f"{IMAGEDIR}{files[random_index]}"
-     
-#     return path
-
-# @app.get("/result/")
-# async def remobe_bg():
-#     files = os.listdir(IMAGEDIR)
-#     random_index = randint(0, len(files) - 1)
- 
-#     path = f"{IMAGEDIR}{files[random_index]}"
-
-#     with open(path, 'rb') as i:
-#         output_path = f"{uuid.uuid4()}.jpg"
-#         with open(f"{IMAGEDIR}{output_path}", 'wb') as o:
-#             input = i.read()
-#             output = remove(input)
-#             o.write(output)
-#     return output_path
 
 
