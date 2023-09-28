72bcf746f37f5d03cd18650070db2184edca60f4
Essadi <yasine.esadi@gmail.com>
2023-09-28 14:15:50 +0100

    handle file location

--- backgournd-remover-server/main.py
+++ backgournd-remover-server/main.py
@@ -19,7 +19,15 @@ async def create_upload_file(file: UploadFile = File(...)):
     contents = await file.read()
     with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
         f.write(contents)
-    return {"filename": file.filename }
+
+    with open(f"{IMAGEDIR}{file.filename}", 'rb') as i:
+        output_path = f"{uuid.uuid4()}.jpg"
+        with open(f"{IMAGEDIR}{output_path}", 'wb') as o:
+            input = i.read()
+            output = remove(input)
+            o.write(output)
+    return output_path
+
 
 # @app.get("/show/")
 # async def read_random_file():
@@ -31,20 +39,20 @@ async def create_upload_file(file: UploadFile = File(...)):
 #     path = f"{IMAGEDIR}{files[random_index]}"
      
 #     return path
-output_path = "ok.png"
-@app.get("/result/")
-async def remobe_bg():
-    files = os.listdir(IMAGEDIR)
-    random_index = randint(0, len(files) - 1)
- 
-    path = f"{IMAGEDIR}{files[random_index]}"
 
-    with open(path, 'rb') as i:
-        output_path = f"{uuid.uuid4()}.jpg"
-        with open(f"{IMAGEDIR}{output_path}", 'wb') as o:
-            input = i.read()
-            output = remove(input)
-            o.write(output)
-    return output_path
+# @app.get("/result/")
+# async def remobe_bg():
+#     files = os.listdir(IMAGEDIR)
+#     random_index = randint(0, len(files) - 1)
+ 
+#     path = f"{IMAGEDIR}{files[random_index]}"
+
+#     with open(path, 'rb') as i:
+#         output_path = f"{uuid.uuid4()}.jpg"
+#         with open(f"{IMAGEDIR}{output_path}", 'wb') as o:
+#             input = i.read()
+#             output = remove(input)
+#             o.write(output)
+#     return output_path
 
 
